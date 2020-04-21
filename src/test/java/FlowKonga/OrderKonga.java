4f351cc59d62c4386491f26a31f85598c6dc9caf
Overflowmike <alabimichael23@gmail.com>
2020-04-21 12:49:56 +0100

    push fourth code commmit

--- src/test/java/FlowKonga/OrderKonga.java
+++ src/test/java/FlowKonga/OrderKonga.java
@@ -21,11 +21,7 @@ public class OrderKonga {
 
         driver.findElement(By.id("https://www.konga.com/account/login?return_url=/")).click();
 
-        driver.findElement(By.id("https://accounts.google.com/signin/oauth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Fwww.konga.com%2Faccount%2Flogin&scope=openid%20profile%20email&client_id=840453489448-es8on0but8hsibv1fmb13odjrae1rv87.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fwww.konga.com&include_granted_scopes=true&origin=https%3A%2F%2Fwww.konga.com&gsiwebsdk=2&access_type=offline&state=%7B%22platform%22%3A%22google%22%2C%22returnUrl%22%3A%22%2F%22%7D&o2v=2&as=l6PVBYG29Vt6CFdRO41kmw&flowName=GeneralOAuthFlow")).click();
-
         driver.findElement(By.label=("Email or phone")).sendKeys("alabimichael23@gmail.com");
-
-        driver.findElement(By.id("Next")).click();
 
         driver.findElement(By.label=("Enter your password")).sendKeys("Samsung@111");
 
@@ -34,12 +30,9 @@ public class OrderKonga {
 }
 
     {
-        driver.findElement(By.id("https://www.konga.com/category/phones-tablets-5294?menu=Phones and Tablets > Mobile")).click();
+        driver.findElement(By.id("https://www.konga.com/category/computers-accessories-5227")).click();
 
 
-        driver.findElement(By.label=("Search for products, brands and categories...")).sendKeys("Samsung");
-
-        driver.findElement(By.id("Searchbutton"))
 
         driver.findElement(By.id("Add To Cart button")).click();
         driver.findElement(By.id("cart")).click();
