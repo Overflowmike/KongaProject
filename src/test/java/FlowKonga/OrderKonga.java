6a75efee61fec35b72ff35794fb06548bba580fa
Overflowmike <alabimichael23@gmail.com>
2020-04-23 13:56:08 +0100

    push update first code commit

--- src/test/java/FlowKonga/OrderKonga.java
+++ src/test/java/FlowKonga/OrderKonga.java
@@ -21,37 +21,13 @@ public class OrderKonga {
 
         driver.findElement(By.id("https://www.konga.com/account/login?return_url=/")).click();
 
-        driver.findElement(By.label=("Email or phone")).sendKeys("alabimichael23@gmail.com");
+        driver.findElement(By.id("Email or phone")).sendKeys("alabimichael23@gmail.com");
 
-        driver.findElement(By.label=("Enter your password")).sendKeys("Samsung@111");
+        driver.findElement(By.id=("Enter your password")).sendKeys("xxxx");
 
         driver.findElement(By.id("Next")).click();
 
 }
-
-    {
-        driver.findElement(By.id("https://www.konga.com/category/computers-accessories-5227")).click();
-
-        driver.findElement(By.id("https://www.konga.com/category/phones-tablets-5294?menu=Phones and Tablets > Mobile")).click();
-
-        driver.findElement(By.label=("Search for products, brands and categories...")).sendKeys("Samsung");
-
-        driver.findElement(By.id("Searchbutton"))
-
-        driver.findElement(By.id("Add To Cart button")).click();
-        driver.findElement(By.id("cart")).click();
-        driver.findElement(By.id("increment")).click();
-        driver.findElement(By.label=("selectPaymentMethod")).sendKeys("kpaygateway">...</button>);
-
-        driver.findElement(By.id("Continue to Payment</button")).click();
-        driver.findElement(By.id("dashboard-card_button Card</button")).click();
-
-        driver.findElement(By.label=("card-number input_class")).sendKeys("5199 0221 11we @as2");
-        driver.findElement(By.label=("date input_class")).sendKeys("05/20");
-        driver.findElement(By.label=("cvv input_class")).sendKeys("776");
-        driver.findElement(By.label=("card-pin")).sendKeys("assed");
-        driver.findElement(By.id("validateCardForm")).click();
-    }
 
 public static void main(String args[]) throws InterruptedException {
         OrderKonga = new OrderKonga();
