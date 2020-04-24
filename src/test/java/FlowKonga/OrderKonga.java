bce5573440190bfd5cddaf5615832cecaaeb9da9
Overflowmike <alabimichael23@gmail.com>
2020-04-24 11:37:34 +0100

    push Select categories code commit

--- src/test/java/FlowKonga/OrderKonga.java
+++ src/test/java/FlowKonga/OrderKonga.java
@@ -21,13 +21,13 @@ public class OrderKonga {
 
         driver.findElement(By.id("https://www.konga.com/account/login?return_url=/")).click();
 
-        driver.findElement(By.id("https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https://www.konga.com/account/login&scope=openid%20profile%20email&client_id=840453489448-es8on0but8hsibv1fmb13odjrae1rv87.apps.googleusercontent.com&ss_domain=https://www.konga.com&include_granted_scopes=true&origin=https://www.konga.com&gsiwebsdk=2&access_type=offline&state={%22platform%22:%22google%22,%22returnUrl%22:%22/%22}")).click();
-
         driver.findElement(By.id("Email or phone")).sendKeys("alabimichael23@gmail.com");
 
         driver.findElement(By.id=("Enter your password")).sendKeys("xxxXxYYY@");
 
         driver.findElement(By.id("Next")).click();
+
+        driver.findElement(By.id("https://www.konga.com/category/computers-accessories-5227")).click();
 
 }
 
