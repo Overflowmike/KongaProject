f205c0aa2ece9650a7c57c657840378a1a2abfc2
Overflowmike <alabimichael23@gmail.com>
2020-04-24 11:27:36 +0100

    push update Konga Facebook login code commit

--- src/test/java/FlowKonga/OrderKonga.java
+++ src/test/java/FlowKonga/OrderKonga.java
@@ -21,7 +21,9 @@ public class OrderKonga {
 
         driver.findElement(By.id("https://www.konga.com/account/login?return_url=/")).click();
 
-        driver.findElement(By.id("Email or phone")).sendKeys("alabimichael23@gmail.com");
+        driver.findElement(By.id("https://www.facebook.com/v3.3/dialog/oauth?client_id=609981359188266&redirect_uri=https://www.konga.com/account/login&response_type=code&scope=email,public_profile&state={%22platform%22:%22facebook%22,%22returnUrl%22:%22/%22}")).click();
+
+        driver.findElement(By.id("Email or phone")).sendKeys("princefmaous01@yahoo.com");
 
         driver.findElement(By.id=("Enter your password")).sendKeys("xxxXxYYY@");
 
